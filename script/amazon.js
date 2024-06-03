1d0cad906563c4808c76efb21320085cb7cab71b
lakshitha <lakshithalkmadushan@gmail.com>
2024-06-03 11:13:14 +0530

    use product.js file

--- script/amazon.js
+++ script/amazon.js
@@ -1,30 +1,4 @@
-const products =[{
-    image :'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating : {
-        star : 4.5,
-        rate : 87
-    },
-    priceCent : 1090,
-},
-{
-    image :'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Intermediate Size Basketball',
-    rating : {
-        star : 4,
-        rate : 127
-    },
-    priceCent : 2095,
-},
-{
-    image :'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating : {
-        star : 4.5,
-        rate : 56
-    },
-    priceCent : 799,
-}]
+
 
 let html ="";
 products.forEach((products) =>{
